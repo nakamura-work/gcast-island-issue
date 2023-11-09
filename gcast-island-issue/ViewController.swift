60f0b51e53eab032f4979ad3596ffe511102c2c6
nakamura-icall <nakamura@webstream.co.jp>
2023-11-09 18:52:24 +0900

    Initial Commit

---
+++ gcast-island-issue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gcast-island-issue
+//
+//  Created by nakamura-icall on 2023/11/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
