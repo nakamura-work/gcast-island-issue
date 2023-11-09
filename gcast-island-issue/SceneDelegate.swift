565d0b7556a376ab51815003d2bab5ae318c9e16
nakamura-icall <nakamura@webstream.co.jp>
2023-11-09 20:43:51 +0900

    Reproduce

--- gcast-island-issue/SceneDelegate.swift
+++ gcast-island-issue/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import GoogleCast
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -16,7 +17,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        let castOptions = GCKCastOptions()
+        GCKCastContext.setSharedInstanceWith(castOptions)
+        if let windowScene = scene as? UIWindowScene {
+            let appStoryboard = UIStoryboard(name: "Main", bundle: nil)
+            let component = appStoryboard.instantiateInitialViewController()! as ViewController
+            let navigationController = UINavigationController(rootViewController: component)
+            
+            let ctx = GCKCastContext.sharedInstance()
+            let castContainerVC = ctx.createCastContainerController(for: navigationController)
+            castContainerVC.miniMediaControlsItemEnabled = true
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = castContainerVC
+            /* For workaround */component.root = castContainerVC
+            self.window = window
+            window.makeKeyAndVisible()
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
--- gcast-island-issue/ViewController.swift
+++ gcast-island-issue/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    /* For workaround */public weak var root: UIViewController?
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.red.cgColor
+        imageView.layer.borderWidth = 1
+        imageView.layer.borderColor = UIColor.blue.cgColor
     }
-
-
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        
+        /* Workaround */
+        //let view = root?.view
+        //view?.setNeedsLayout()
+        //view?.layoutIfNeeded()
+        /* Workaround */
+    }
 }
 
